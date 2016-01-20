6fda6254265a8e21af68d7179ded5a41cd2f0186
Buck DeFore <bdefore@gmail.com>
2016-01-19 22:31:25 -0800

    update to latest with redux-async-connect

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routeReducer } from 'redux-simple-router';
+import { reducer as reduxAsyncConnect } from 'redux-async-connect';
 import auth from './auth';
 import api from './api';
 
 export default combineReducers({
   auth: auth,
   api: api,
+  reduxAsyncConnect,
   routing: routeReducer
 });
