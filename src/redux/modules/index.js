76957b872f3d99d09c3239d5cd6531308fd6d38c
Buck DeFore <bdefore@gmail.com>
2016-07-26 15:16:13 -0400

    update UR

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { routeReducer } from 'react-router-redux';
-import { reducer as reduxAsyncConnect } from 'redux-async-connect';
+import { reducer as reduxAsyncConnect } from 'fmp-redux-async-connect';
 import auth from './auth';
 import api from './api';
 
